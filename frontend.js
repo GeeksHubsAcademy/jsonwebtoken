9604f11cc376cdc530584bc4f95edcd601ef7f69
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-06-27 11:14:26 +0200

    added fetch request

---
+++ frontend.js
@@ -0,0 +1,20 @@
+fetch('http://localhost:3001/login')
+.then(res=>res.json())
+.then(res=>{
+    console.log(res.token);
+    localStorage.setItem('authToken',res.token)
+})
+.catch(console.log)
+
+fetch(`http://localhost:3001/updateProfile/${userID}`,{
+    headers:{
+        authenticate:localStorage.getItem('authToken'),
+        "Content-Type":"application/json"
+    },
+    method:"PATCH",
+    body:JSON.stringify({
+        nombre:"nuevoNombre"
+    })
+})
+.then()
+.catch(console.log)
--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require( 'express' ); // importamos la liberia express
 const app = express(); //generamos la app de backend a partir de express
 const jwt = require( 'jsonwebtoken' );
 
+app.use( function ( req, res, next ) {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authenticate" );
+    res.header( "Access-Control-Allow-Methods", "POST, PATCH, PUT, DELETE" );
+    next();
+} );
+app.use( express.json() )
+
 app.get( '/login', ( req, res ) => {
 
-    const token = jwt.sign( { id: 587,
-     }, "a5sd19d1sa98ds89aads8", { expiresIn: "7d" } ) //generamos el token
-    console.log( token )
-    const payload = jwt.verify( token, "a5sd19d1sa98ds89aads8" ) //aquí extraigo el payload del token utilizando el secreto
-    res.send( token, payload );
+    const token = jwt.sign( {
+        id: 587,
+    }, "a5sd19d1sa98ds89aads8", { expiresIn: "7d" } ) //generamos el token
+    res.json( { token } );
 } )
-
+app.patch( '/updateProfile/', (req,res) => {
+    console.log( req.headers.authenticate );
+    console.log(req.body)
+} )
 app.listen( 3001, () => console.log( 'servidor levantado' ) ) //aquí ponemos el servidor a la escucha de peticiones en el puerto 3001
