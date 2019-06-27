d11f9e5f14073ee76edaf81db601c6c0c28ea470
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-06-27 10:52:29 +0200

    token generated and token decoded

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express=require('express');// importamos la liberia express
-const app=express();//generamos la app de backend a partir de express
-const jwt=require('jsonwebtoken');
+const express = require( 'express' ); // importamos la liberia express
+const app = express(); //generamos la app de backend a partir de express
+const jwt = require( 'jsonwebtoken' );
 
-app.get('/',(req,res)=>{
-    const token=jwt.sign({id:587},"12345",{expiresIn:"7d"})
-})
+app.get( '/login', ( req, res ) => {
 
-app.listen(3001,()=>console.log('servidor levantado'))//aquí ponemos el servidor a la escucha de peticiones en el puerto 3001
+    const token = jwt.sign( { id: 587,
+     }, "a5sd19d1sa98ds89aads8", { expiresIn: "7d" } ) //generamos el token
+    console.log( token )
+    const payload = jwt.verify( token, "a5sd19d1sa98ds89aads8" ) //aquí extraigo el payload del token utilizando el secreto
+    res.send( token, payload );
+} )
+
+app.listen( 3001, () => console.log( 'servidor levantado' ) ) //aquí ponemos el servidor a la escucha de peticiones en el puerto 3001
