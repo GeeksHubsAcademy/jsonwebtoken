628178152e5321292fd037949f49889ff11bf246
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-06-27 10:17:35 +0200

    package-lock ignored

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();//generamos la app de backend a partir de express
 const jwt=require('jsonwebtoken');
 
 app.get('/',(req,res)=>{
-    const token=jwt.sign({id:587},"a1sd891a8sda89s")
+    const token=jwt.sign({id:587},"12345",{expiresIn:"7d"})
 })
 
 app.listen(3001,()=>console.log('servidor levantado'))//aquí ponemos el servidor a la escucha de peticiones en el puerto 3001
